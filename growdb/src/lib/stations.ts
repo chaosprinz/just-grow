38d5a73229a7b1b900e2df09cac25b1b12119473
Siggi Duenkel <chaosprinz76@googlemail.com>
2024-07-04 13:01:19 +0200

    option to create new stationTypes when creating stations

--- growdb/src/lib/stations.ts
+++ growdb/src/lib/stations.ts
@@ -1,6 +1,22 @@
 import { stations as stationsTable } from '../db/schema'
+import db from './db'
 import createTableApi from './helper/createTableApi'
+import stationTypes from './stationTypes'
 
 const stations = createTableApi(stationsTable)
+
+stations.createOne = (data: any): Record<string, unknown> => {
+  if (!data.newStationType) {
+    return db.insert(stationsTable).values(data).returning().get()
+  }
+
+  const stationType = stationTypes.createOne(data.newStationType)
+
+  return db
+    .insert(stationsTable)
+    .values({...data, stationTypeId: stationType.id})
+    .returning()
+    .get()
+}
 
 export default stations
