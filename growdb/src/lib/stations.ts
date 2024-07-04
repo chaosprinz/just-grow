64ac7adf2ffc778ea6af1c8869b5b8b77db866f4
Siggi Duenkel <chaosprinz76@googlemail.com>
2024-07-04 13:17:09 +0200

    Added option to create new station type when updating a station.

--- growdb/src/lib/stations.ts
+++ growdb/src/lib/stations.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm'
 import { stations as stationsTable } from '../db/schema'
 import db from './db'
 import createTableApi from './helper/createTableApi'
@@ -7,7 +8,11 @@ const stations = createTableApi(stationsTable)
 
 stations.createOne = (data: any): Record<string, unknown> => {
   if (!data.newStationType) {
-    return db.insert(stationsTable).values(data).returning().get()
+    return db
+      .insert(stationsTable)
+      .values(data)
+      .returning()
+      .get()
   }
 
   const stationType = stationTypes.createOne(data.newStationType)
@@ -15,6 +20,26 @@ stations.createOne = (data: any): Record<string, unknown> => {
   return db
     .insert(stationsTable)
     .values({...data, stationTypeId: stationType.id})
+    .returning()
+    .get()
+}
+
+stations.updateOne = (id, data: any): Record<string, unknown> => {
+  if (!data.newStationType) {
+    return db
+      .update(stationsTable)
+      .set(data)
+      .where(eq(stationsTable.id, data))
+      .returning()
+      .get()
+  }
+
+  const stationType = stationTypes.createOne(data.newStationType)
+
+  return db
+    .update(stationsTable)
+    .set({...data, stationTypeId: stationType.id})
+    .where(eq(stationsTable.id, id))
     .returning()
     .get()
 }
