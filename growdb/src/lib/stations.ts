8cc7442a6dde144aa7b4784a76552d747f8ac501
Siggi Duenkel <chaosprinz76@googlemail.com>
2024-07-04 15:40:59 +0200

    Added option to get many stations joined by their type

--- growdb/src/lib/stations.ts
+++ growdb/src/lib/stations.ts
@@ -18,6 +18,25 @@ const selectJoinedStations = () => db
 
 const stations = {
   ..._stations,
+  getMany: (withStationType: boolean = true) => {
+    if (!withStationType) return _stations.getMany()
+    
+    return selectJoinedStations()
+      .all()
+      .reduce<Record<number, { id: number; name: string; stationType: any}>>(
+        (acc, row) => {
+          const { id, name, stationType } = row
+          
+          if (!acc[id]) acc[id] = { id, name, stationType: {}}
+
+          if (stationType) acc[id].stationType = stationType
+
+          return acc
+        },
+        {}
+      )
+  },
+
   getOne: (id: number, withStationType: boolean = true): Record<string, unknown> | undefined => {
     if (!withStationType) return _stations.getOne(id)
 
