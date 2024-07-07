13c98e458861188b5c4a2bb6758630d05982bd4e
Siggi Duenkel <chaosprinz76@googlemail.com>
2024-07-07 14:35:09 +0200

    extended and documented stations-api
    - added upsert function (insert or update)
    - add doc-strings

--- growdb/src/lib/stations.ts
+++ growdb/src/lib/stations.ts
@@ -17,6 +17,12 @@ const _stations = createTableApi(stationsTable)
 const stations = {
   ..._stations,
 
+  /**
+   * Retrieves multiple stations from the database based on the provided options.
+   *
+   * @param {Object} [options={withStationType: true}] - Optional parameters for retrieving the stations.
+   * @return {Promise<Station[]>} A promise that resolves to an array of stations.
+   */
   getMany: (options = {withStationType: true}) => db.query.stations
     .findMany({
       with: {
@@ -24,6 +30,14 @@ const stations = {
       }
     }),
 
+  /**
+   * Retrieves a single station from the database based on the provided ID.
+   *
+   * @param {number} id - The ID of the station to retrieve.
+   * @param {Object} [options] - Optional parameters for retrieving the station.
+   * @param {boolean} [options.withStationType=true] - Whether to include the station type in the result.
+   * @return {Promise<Station | null>} A promise that resolves to the retrieved station, or null if not found.
+   */
   getOne: (id: number, options = {withStationType: true}) => db.query.stations
     .findFirst({
       where: eq(stationsTable.id, id),
@@ -32,6 +46,12 @@ const stations = {
       }
     }),
 
+    /**
+     * Creates a new station in the database.
+     *
+     * @param {StationData} data - The data for creating the station.
+     * @return {Promise<Station>} A promise that resolves to the created station.
+     */
   createOne: (data: StationData) => {
     if (!data.newStationType) return _stations.createOne(data)
 
@@ -44,15 +64,42 @@ const stations = {
       .get()      
   },
 
+  /**
+   * A function that updates a station's information in the database.
+   *
+   * @param {number} id - The ID of the station to update.
+   * @param {StationData} data - The new data for the station.
+   * @return {Promise<Station>} A promise that resolves to the updated station data.
+   */
   updateOne: (id: number, data: StationData) => {
     if (!data.newStationType) return _stations.updateOne(id, data)
     
-    console.log(data)
     const stationType = stationTypes.createOne(data.newStationType)
     return db
       .update(stationsTable)
       .set({...data, stationTypeId: stationType.id})
       .where(eq(stationsTable.id, id))
+      .returning()
+      .get()
+  },
+  
+  /**
+   * Inserts a new station into the database or updates an existing one if it already exists.
+   *
+   * @param {StationData} data - The data of the station to be inserted or updated.
+   * @return {Promise<Station>} A promise that resolves to the inserted or updated station.
+   */
+  upsertOne: (data: StationData) => {
+    if (!data.newStationType) return  _stations.createOne(data)
+
+    const stationType = stationTypes.createOne(data.newStationType)
+    return db
+      .insert(stationsTable)
+      .values({...data, stationTypeId: stationType.id })
+      .onConflictDoUpdate({
+        target: [stationsTable.id],
+        set: data
+      })
       .returning()
       .get()
   }
